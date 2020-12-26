3d405b307ed391eddda63383be659913748d03f2
jackalsin <jackal.sin@hotmail.com>
2020-12-25 21:37:39 -0800

    Add constants.go

---
+++ constants.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const Pi = 3.14
+
+func main() {
+	const world = "驶接"
+	fmt.Println("hello", "World")
+	fmt.Println("happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
